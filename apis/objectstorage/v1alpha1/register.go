ae86be815abcc27f80bc89164932675ea007f7fe
Akash Mukhopadhyay <akmukhop@microsoft.com>
2022-07-08 08:35:39 -0700

    Referencing objectstorage groupname from v1alpha1 package

--- apis/objectstorage/v1alpha1/register.go
+++ apis/objectstorage/v1alpha1/register.go
@@ -19,11 +19,12 @@ package v1alpha1
 import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"sigs.k8s.io/controller-runtime/pkg/scheme"
+	objectstorage "sigs.k8s.io/container-object-storage-interface-api/apis/objectstorage"
 )
 
 var (
 	// GroupVersion is group version used to register these objects
-	SchemeGroupVersion = schema.GroupVersion{Group: "objectstorage.k8s.io", Version: "v1alpha1"}
+	SchemeGroupVersion = schema.GroupVersion{Group: objectstorage.GroupName, Version: "v1alpha1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: SchemeGroupVersion}

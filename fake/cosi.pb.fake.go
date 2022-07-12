abaa1521d2354319399d47b6436db29f833dfd57
Akash Mukhopadhyay <akmukhop@microsoft.com>
2022-07-12 15:09:51 -0700

    Updating the auto generated files

--- fake/cosi.pb.fake.go
+++ fake/cosi.pb.fake.go
@@ -7,29 +7,29 @@ import (
 )
 
 type FakeIdentityClient struct {
-	FakeProvisionerGetInfo func(ctx context.Context, in *containerobjectstorageinterfacespec.ProvisionerGetInfoRequest, opts ...grpc.CallOption) (*containerobjectstorageinterfacespec.ProvisionerGetInfoResponse, error)
+	FakeDriverGetInfo func(ctx context.Context, in *containerobjectstorageinterfacespec.DriverGetInfoRequest, opts ...grpc.CallOption) (*containerobjectstorageinterfacespec.DriverGetInfoResponse, error)
 }
 
-func (f *FakeIdentityClient) ProvisionerGetInfo(ctx context.Context, in *containerobjectstorageinterfacespec.ProvisionerGetInfoRequest, opts ...grpc.CallOption) (*containerobjectstorageinterfacespec.ProvisionerGetInfoResponse, error) {
-	return f.FakeProvisionerGetInfo(ctx, in, opts...)
+func (f *FakeIdentityClient) DriverGetInfo(ctx context.Context, in *containerobjectstorageinterfacespec.DriverGetInfoRequest, opts ...grpc.CallOption) (*containerobjectstorageinterfacespec.DriverGetInfoResponse, error) {
+	return f.FakeDriverGetInfo(ctx, in, opts...)
 }
 
 type FakeProvisionerClient struct {
-	FakeProvisionerCreateBucket       func(ctx context.Context, in *containerobjectstorageinterfacespec.ProvisionerCreateBucketRequest, opts ...grpc.CallOption) (*containerobjectstorageinterfacespec.ProvisionerCreateBucketResponse, error)
-	FakeProvisionerDeleteBucket       func(ctx context.Context, in *containerobjectstorageinterfacespec.ProvisionerDeleteBucketRequest, opts ...grpc.CallOption) (*containerobjectstorageinterfacespec.ProvisionerDeleteBucketResponse, error)
-	FakeProvisionerGrantBucketAccess  func(ctx context.Context, in *containerobjectstorageinterfacespec.ProvisionerGrantBucketAccessRequest, opts ...grpc.CallOption) (*containerobjectstorageinterfacespec.ProvisionerGrantBucketAccessResponse, error)
-	FakeProvisionerRevokeBucketAccess func(ctx context.Context, in *containerobjectstorageinterfacespec.ProvisionerRevokeBucketAccessRequest, opts ...grpc.CallOption) (*containerobjectstorageinterfacespec.ProvisionerRevokeBucketAccessResponse, error)
+	FakeDriverCreateBucket       func(ctx context.Context, in *containerobjectstorageinterfacespec.DriverCreateBucketRequest, opts ...grpc.CallOption) (*containerobjectstorageinterfacespec.DriverCreateBucketResponse, error)
+	FakeDriverDeleteBucket       func(ctx context.Context, in *containerobjectstorageinterfacespec.DriverDeleteBucketRequest, opts ...grpc.CallOption) (*containerobjectstorageinterfacespec.DriverDeleteBucketResponse, error)
+	FakeDriverGrantBucketAccess  func(ctx context.Context, in *containerobjectstorageinterfacespec.DriverGrantBucketAccessRequest, opts ...grpc.CallOption) (*containerobjectstorageinterfacespec.DriverGrantBucketAccessResponse, error)
+	FakeDriverRevokeBucketAccess func(ctx context.Context, in *containerobjectstorageinterfacespec.DriverRevokeBucketAccessRequest, opts ...grpc.CallOption) (*containerobjectstorageinterfacespec.DriverRevokeBucketAccessResponse, error)
 }
 
-func (f *FakeProvisionerClient) ProvisionerCreateBucket(ctx context.Context, in *containerobjectstorageinterfacespec.ProvisionerCreateBucketRequest, opts ...grpc.CallOption) (*containerobjectstorageinterfacespec.ProvisionerCreateBucketResponse, error) {
-	return f.FakeProvisionerCreateBucket(ctx, in, opts...)
+func (f *FakeProvisionerClient) DriverCreateBucket(ctx context.Context, in *containerobjectstorageinterfacespec.DriverCreateBucketRequest, opts ...grpc.CallOption) (*containerobjectstorageinterfacespec.DriverCreateBucketResponse, error) {
+	return f.FakeDriverCreateBucket(ctx, in, opts...)
 }
-func (f *FakeProvisionerClient) ProvisionerDeleteBucket(ctx context.Context, in *containerobjectstorageinterfacespec.ProvisionerDeleteBucketRequest, opts ...grpc.CallOption) (*containerobjectstorageinterfacespec.ProvisionerDeleteBucketResponse, error) {
-	return f.FakeProvisionerDeleteBucket(ctx, in, opts...)
+func (f *FakeProvisionerClient) DriverDeleteBucket(ctx context.Context, in *containerobjectstorageinterfacespec.DriverDeleteBucketRequest, opts ...grpc.CallOption) (*containerobjectstorageinterfacespec.DriverDeleteBucketResponse, error) {
+	return f.FakeDriverDeleteBucket(ctx, in, opts...)
 }
-func (f *FakeProvisionerClient) ProvisionerGrantBucketAccess(ctx context.Context, in *containerobjectstorageinterfacespec.ProvisionerGrantBucketAccessRequest, opts ...grpc.CallOption) (*containerobjectstorageinterfacespec.ProvisionerGrantBucketAccessResponse, error) {
-	return f.FakeProvisionerGrantBucketAccess(ctx, in, opts...)
+func (f *FakeProvisionerClient) DriverGrantBucketAccess(ctx context.Context, in *containerobjectstorageinterfacespec.DriverGrantBucketAccessRequest, opts ...grpc.CallOption) (*containerobjectstorageinterfacespec.DriverGrantBucketAccessResponse, error) {
+	return f.FakeDriverGrantBucketAccess(ctx, in, opts...)
 }
-func (f *FakeProvisionerClient) ProvisionerRevokeBucketAccess(ctx context.Context, in *containerobjectstorageinterfacespec.ProvisionerRevokeBucketAccessRequest, opts ...grpc.CallOption) (*containerobjectstorageinterfacespec.ProvisionerRevokeBucketAccessResponse, error) {
-	return f.FakeProvisionerRevokeBucketAccess(ctx, in, opts...)
+func (f *FakeProvisionerClient) DriverRevokeBucketAccess(ctx context.Context, in *containerobjectstorageinterfacespec.DriverRevokeBucketAccessRequest, opts ...grpc.CallOption) (*containerobjectstorageinterfacespec.DriverRevokeBucketAccessResponse, error) {
+	return f.FakeDriverRevokeBucketAccess(ctx, in, opts...)
 }

be28c72872a81132886529c8181f893e632c0b61
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2023-11-30 19:36:00 +0100

    feat(events): added volume controllers tyle events package
    Signed-off-by: Mateusz Urbanek <mateusz.urbanek.98@gmail.com>

---
+++ controller/events/events.go
@@ -0,0 +1,10 @@
+package events
+
+// COSI relevant event reasons
+const (
+	BucketDeleteFailed   = "BucketDeleteFailed"
+	GrantingAccessFailed = "GrantingAccessFailed"
+	ProvisioningFailed   = "ProvisioningFailed"
+	RevokingAccessFailed = "RevokingAccessFailed"
+	WaitingForBucket     = "WaitingForBucket"
+)
